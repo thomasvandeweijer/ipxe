6df8bc5538f05b6373e9d20fd22711c9fd18b84a
Jeroen Bulten <jeroen.bulten@sidn.nl>
2023-12-08 17:09:03 +0100

    let get-network-config.sh retry a few times using a loop with expontential back off

--- examples/get-network-config.sh
+++ examples/get-network-config.sh
@@ -25,21 +25,28 @@ get_external_info() {
 
 }
 
-read -r EXT_IF4 DEF_GW4 GLOBAL_IP4 <<<"$(get_external_info 4)"
-read -r EXT_IF6 DEF_GW6 GLOBAL_IP6 <<<"$(get_external_info 6)"
+modified_fibonacci=(2 3 5 8 13 13 21 21 34 ) # start with fast retry then exponentially back off
+
+for i in "${modified_fibonacci[@]}"
+do
+    read -r EXT_IF4 DEF_GW4 GLOBAL_IP4 <<<"$(get_external_info 4)"
+    read -r EXT_IF6 DEF_GW6 GLOBAL_IP6 <<<"$(get_external_info 6)"
+
+    # see if any values came back empty
+    for j in EXT_IF4 DEF_GW4 GLOBAL_IP4 DEF_GW6 GLOBAL_IP6; do
+        if [ -z "${!j}" ]; then
+           >&2 echo "Error getting value $i from ip route info. Retrying after $i seconds"
+           continue
+        fi
+    done
+    break # all values were succesfully retrieved
+done
+
 
 if [ "${EXT_IF4}" != "${EXT_IF6}" ]; then
     >&2 echo "External interface for IPv4 is different from the external interface for IPv6!"
     exit 1
 fi
-
-# exit with an error if one of the keys returns an empty value
-for i in EXT_IF4 DEF_GW4 GLOBAL_IP4 DEF_GW6 GLOBAL_IP6; do
-    if [ -z "${!i}" ]; then
-       >&2 echo "Error getting value $i from ip route info"
-       exit 1
-    fi
-done
 
 for i in EXT_IF4 DEF_GW4 GLOBAL_IP4 DEF_GW6 GLOBAL_IP6; do
     echo "${i}=${!i}"
